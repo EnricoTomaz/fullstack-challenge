bb1f98a5083791785ecabb54649696ec7b22401a
Enrico <eru.tomaz@gmail.com>
2022-05-05 04:21:34 -0300

    removing bug

--- server/api/router.js
+++ server/api/router.js
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import ProfessionalController from './controllers/professionalController';
+import ProfessionalController from './controllers/ProfessionalController';
 
 export default express
   .Router()
